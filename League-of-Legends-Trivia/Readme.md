470d93ef27d36dbcadd7d6054ef562e9b6fddf37
Isaac Sheikh <sheikhisaac@gmail.com>
2017-07-18 09:28:47 -0400

    Add files via upload

---
+++ League-of-Legends-Trivia/Readme.md
@@ -0,0 +1,6 @@
+This is a basic trivia game I created for Amazon's Alexa.
+
+It includes an index class to handle everything, a questions class to import any number of trivia questions,
+and the necessary intent schema and sample utterances to make it work.
+
+The content of the trivia is focused around Riot Games' League of Legends.
---
+++ League-of-Legends-Trivia/src/questions.js
@@ -0,0 +1,190 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        //1
+        {
+            "Who is the Glorious Executioner": [
+                "Draven",
+                "Aatrox",
+                "Katarina",
+                "Zed"
+            ]
+        },
+        //2
+        {
+            "What two champions ignore unit collision?": [
+                "Fizz and Kassadin",
+                "Gnar and Fizz",
+                "Yasuo and Vayne",
+                "Talon and Teemo"
+            ]
+        },
+        //3
+        {
+            "Who is the Blade of Noxus": [
+                "Katarina",
+                "Draven",
+                "Darius",
+                "Riven"
+            ]
+        },
+        //4
+        {
+            "Who is the Exile": [
+                "Riven",
+                "Urgot",
+                "Katarina",
+                "Zed"
+            ]
+        },
+        //5
+        {
+            "What champion uses their own health to heal allies": [
+                "Soraka",
+                "Sona",
+                "Nami",
+                "Taliyah"
+            ]
+        },
+        //6
+        {
+            "What champion can control dragons": [
+                "Mordekaiser",
+                "Dr. Mundo",
+                "Galio",
+                "Veigar"
+            ]
+        },
+        //7
+        {
+            "Who can create a clone of themselves to deceive enemies": [
+                "Leblanc",
+                "Ekko",
+                "Karma",
+                "Malzahar"
+            ]
+        },
+        //8
+        {
+            "Who is the Night Hunter": [
+                "Vayne",
+                "Varus",
+                "Shen",
+                "Kog'maw"
+            ]
+        },
+        //9
+        {
+            "Who collects chimes": [
+                "Bard",
+                "Sona",
+                "Poppy",
+                "Ivern"
+            ]
+        },
+        //10
+        {
+            "Who was Gangplank's first love": [
+                "Illaoi",
+                "Miss Fortune",
+                "Swain",
+                "Nautilus"
+            ]
+        },
+        //11
+        {
+            "Who is the Bounty Hunter": [
+                "Miss Fortune",
+                "Talon",
+                "Sion",
+                "Nasus"
+            ]
+        },
+        //12
+        {
+            "Who is the Outlaw": [
+                "Graves",
+                "Riven",
+                "Katarina",
+                "Miss Fortune"
+            ]
+        },
+        //13
+        {
+            "What skin can you win at an official tournament": [
+                "Triumphant Ryze",
+                "Riot Graves",
+                "PAX Twisted Fate",
+                "Riot Girl Tristana"
+            ]
+        },
+        //14
+        {
+            "Who is the Madman of Zaun": [
+                "Dr. Mundo",
+                "Viktor",
+                "Singed",
+                "Urgot"
+            ]
+        },
+        //15
+        {
+            "Who is the Defender of Tomorrow": [
+                "Jayce",
+                "Aatrox",
+                "Ezreal",
+                "Jarvan"
+            ]
+        },
+        //16
+        {
+            "What champion can use an ally as a projectile": [
+                "Kalista",
+                "Hecarim",
+                "Mordekaiser",
+                "Yorick"
+            ]
+        },
+        //17
+        {
+            "Who is the Piltover Enforcer": [
+                "Vi",
+                "Caitlyn",
+                "Rumble",
+                "Jayce"
+            ]
+        },
+        //18
+        {
+            "What champion just wants a hug": [
+                "Amumu",
+                "Annie",
+                "Urgot",
+                "Blitzcrank"
+            ]
+        },
+        //19
+        {
+            "Who is fighting for his lost wife": [
+                "Lucian",
+                "Corki",
+                "Brand",
+                "Nunu"
+            ]
+        },
+        //20
+        {
+            "Who is the Fallen Angel": [
+                "Morgana",
+                "Kayle",
+                "Sivir",
+                "Lux"
+            ]
+        }
+    ]
+};
